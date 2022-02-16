2ca0d4c83efbf34e1b646383fae8966255a3198b
Robert Wagner <rwwagner90@gmail.com>
2022-02-16 11:32:28 -0500

    Start implementing design

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,18 @@
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,svelte,ts,tsx,vue}'],
+  content: ['./src/**/*.{astro,html,js,jsx,svelte,ts,tsx,vue}'],
+  theme: {
+    fontFamily: {
+      body: ['Surt'],
+      heading: ['Bologna']
+    },
+    extend: {
+      colors: {
+        cream: '#E5E5E5',
+        'dark-green': '#1D2D2E',
+        gold: '#AA7A4E',
+        'light-gold': '#E7DFD1',
+				white: '#EFE9DF'
+      }
+    }
+  }
 };
